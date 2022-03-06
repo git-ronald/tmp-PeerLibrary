c6c67bd36b99a5f5370b2cce2026c8577a1316ae
git-ronald <inbox.ronald@outlook.com>
2022-03-06 22:32:57 +0100

    ISchedulerConfig at Peer level

---
+++ Scheduler/PeerCompartmentSchedulerConfig.cs
@@ -0,0 +1,35 @@
+﻿using CoreLibrary;
+using CoreLibrary.SchedulerService;
+
+namespace PeerLibrary.Scheduler
+{
+    public class PeerCompartmentSchedulerConfig : ISchedulerConfig<object, TimeCompartments>
+    {
+        public PeerCompartmentSchedulerConfig()
+        {
+            Tasks[TimeCompartments.EveryMinute] = new()
+            {
+                ScheduleEveryMinute
+            };
+
+            Tasks[TimeCompartments.Every2Minutes] = new()
+            {
+                ScheduleEvery2Minutes
+            };
+        }
+
+        public Dictionary<TimeCompartments, List<ScheduledTaskDelegate<object>>> Tasks { get; } = new();
+
+        private Task ScheduleEveryMinute(CancellationToken stoppingToken, object? state)
+        {
+            Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss} Every minute by PeerLibrary");
+            return Task.CompletedTask;
+        }
+
+        private Task ScheduleEvery2Minutes(CancellationToken stoppingToken, object? state)
+        {
+            Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss} Every 2 minutes by PeerLibrary");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Scheduler/PeerFixedTimeSchedulerConfig.cs
@@ -0,0 +1,21 @@
+﻿using CoreLibrary.SchedulerService;
+
+namespace PeerLibrary.Scheduler
+{
+    public class PeerFixedTimeSchedulerConfig : ISchedulerConfig<object, TimeSpan>
+    {
+        public PeerFixedTimeSchedulerConfig()
+        {
+            Tasks[new TimeSpan(21, 15, 0)] = new()
+            {
+                (token, state) =>
+                {
+                    Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss} Fixed time by PeerLibrary");
+                    return Task.CompletedTask;
+                }
+            };
+        }
+
+        public Dictionary<TimeSpan, List<ScheduledTaskDelegate<object>>> Tasks { get; } = new();
+    }
+}
